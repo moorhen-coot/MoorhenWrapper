d4871f7783609a03ce8a508cde1d3d0e4f8a76d4
Filomeno Sanchez <filomeno.sanchezrodriguez@york.ac.uk>
2023-11-07 17:30:48 +0000

    remove unused props from exit menu

--- src/components/NavBarMenus/MoorhenExitMenu.tsx
+++ src/components/NavBarMenus/MoorhenExitMenu.tsx
@@ -1,10 +1,6 @@
 import { useEffect } from "react";
-import { moorhen } from "moorhen/types/moorhen";
-import { webGL } from "moorhen/types/mgWebGL";
 
 export const MoorhenExitMenu = (props: {
-    molecules: moorhen.Molecule[];
-    glRef: React.RefObject<webGL.MGWebGL>;
     exitCallback: () => Promise<void>;
 }) => {
 
