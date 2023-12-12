2bf26538a3858d793d12ed022e411e6df1ce2013
Filomeno Sanchez <filomeno.sanchezrodriguez@york.ac.uk>
2023-12-12 17:56:16 +0000

    comment out MR mode for now

--- src/components/MoorhenCloudApp.tsx
+++ src/components/MoorhenCloudApp.tsx
@@ -1,12 +1,10 @@
 import { useRef, useState, useEffect } from 'react'
-import { MoorhenContainer, MoorhenQuerySequenceModal } from "moorhen"
+import { MoorhenContainer } from "moorhen"
 import { MoorhenLegendToast } from './misc/MoorhenLegendToast'
 import { MoorhenExitMenu } from "./NavBarMenus/MoorhenExitMenu"
-import { MoorhenMrFetchOnlineSources } from "./NavBarMenus/MoorhenMrFetchOnlineSources"
-import { MoorhenSliceNDiceModal } from "./modals/MoorhenSliceNDiceModal"
 import { moorhen } from "moorhen/types/moorhen"
 import { webGL } from "moorhen/types/mgWebGL"
-import { CloudDownloadOutlined, ContentCutOutlined, LogoutOutlined, ManageSearchOutlined } from '@mui/icons-material'
+import { LogoutOutlined } from '@mui/icons-material'
 import { useDispatch, useSelector } from 'react-redux';
 import { AnyAction, Dispatch } from "@reduxjs/toolkit";
 
@@ -38,15 +36,10 @@ export const MoorhenCloudApp = (props: MoorhenCloudAppPropsInterface) => {
     const mapsRef = useRef<moorhen.Map[] | null>(null)
     const activeMapRef = useRef<moorhen.Map | null>(null)
     const exitDialActionRef = useRef(null)
-    const seqSearchDialActionRef = useRef(null)
-    const fetchDialActionRef = useRef(null)
-    const showSliceNDiceDialActionRef = useRef(null)
     
     const [legendText, setLegendText] = useState<string | JSX.Element>('Loading, please wait...')
     const [busyFetching, setBusyFetching] = useState<boolean>(false)
     const [notifyNewContent, setNotifyNewContent] = useState<boolean>(false)
-    const [showMrQuerySequenceModal, setShowMrQuerySequenceModal] = useState<boolean>(false)
-    const [showSliceNDiceModal, setShowSliceNDiceModal] = useState<boolean>(false)
 
     const dispatch = useDispatch()
     const cootInitialized = useSelector((state: moorhen.State) => state.generalStates.cootInitialized)
@@ -55,6 +48,14 @@ export const MoorhenCloudApp = (props: MoorhenCloudAppPropsInterface) => {
     let extraNavBarMenus = []
     let extraNavBarModals = []
     
+    /**
+    const seqSearchDialActionRef = useRef(null)
+    const fetchDialActionRef = useRef(null)
+    const showSliceNDiceDialActionRef = useRef(null)
+
+    const [showMrQuerySequenceModal, setShowMrQuerySequenceModal] = useState<boolean>(false)
+    const [showSliceNDiceModal, setShowSliceNDiceModal] = useState<boolean>(false)
+
     if (props.isMrPreparationMode) {
         includeNavBarMenuNames = ["Fetch coords", "Seq. search", "Slice-n-Dice", "Models", "View", "Preferences", "Help", "Exit"]
         extraNavBarMenus.push({
@@ -97,7 +98,7 @@ export const MoorhenCloudApp = (props: MoorhenCloudAppPropsInterface) => {
             }, 
         )
     }
-    
+    */    
     extraNavBarMenus.push({
         icon: <LogoutOutlined/>,
         name: 'Exit',
